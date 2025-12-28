a7104c1280f5174393a2949f45f797f08f51acfe
nguyenvanbao <nguyenhoanggiaphong12@gmail.com>
2025-12-29 00:03:18 +0700

    feat: implement warehouse management features including CRUD operations and routing

--- erp-frontend/src/features/inventory/api/warehouse.api.ts
+++ erp-frontend/src/features/inventory/api/warehouse.api.ts
@@ -1,7 +1,26 @@
 import axiosClient from "../../../api/axiosClient";
+import { WarehouseDTO } from "../store";
 export const warehouseApi = {
   getAllWarehouses: async () => {
     const res = await axiosClient.get("/warehouse");
+    return res.data;
+  },
+  getWarehouseById: async (id: number) => {
+    const res = await axiosClient.get(`/warehouse/${id}`);
+    return res.data;
+  },
+  createWarehouse: async (data: WarehouseDTO) => {
+    const res = await axiosClient.post("/warehouse", data);
+    return res.data;
+  },
+
+  updateWarehouse: async (id: number, data: WarehouseDTO) => {
+    const res = await axiosClient.put(`/warehouse/${id}`, data);
+    return res.data;
+  },
+
+  deleteWarehouse: async (id: number) => {
+    const res = await axiosClient.delete(`/warehouse/${id}`);
     return res.data;
   },
 };
--- erp-frontend/src/features/inventory/services/warehouse.service.ts
+++ erp-frontend/src/features/inventory/services/warehouse.service.ts
@@ -1,10 +1,43 @@
 import { warehouseApi } from "../api/warehouse.api";
 import { getErrorMessage } from "../../../utils/ErrorHelper";
+import { WarehouseDTO } from "../store/stock/warehouse/warehouse.types";
 
 export const warehouseService = {
   async getAllWarehouses() {
     try {
       return await warehouseApi.getAllWarehouses();
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  },
+
+  async getWarehouseById(id: number) {
+    try {
+      return await warehouseApi.getWarehouseById(id);
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  },
+
+  async createWarehouse(data: WarehouseDTO) {
+    try {
+      return await warehouseApi.createWarehouse(data);
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  },
+
+  async updateWarehouse(id: number, data: WarehouseDTO) {
+    try {
+      return await warehouseApi.updateWarehouse(id, data);
+    } catch (error) {
+      throw new Error(getErrorMessage(error));
+    }
+  },
+
+  async deleteWarehouse(id: number) {
+    try {
+      return await warehouseApi.deleteWarehouse(id);
     } catch (error) {
       throw new Error(getErrorMessage(error));
     }
